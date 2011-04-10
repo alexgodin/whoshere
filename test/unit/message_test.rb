153d7f02fde1be0ad6f950d9efdcf0b882fd49b4
Alex <alex@alexgodin.com>
2011-04-10 08:18:38 -0400

    .

--- test/unit/message_test.rb
+++ test/unit/message_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class MessagesTest < ActiveSupport::TestCase
+class MessageTest < ActiveSupport::TestCase
   # Replace this with your real tests.
   test "the truth" do
     assert true
