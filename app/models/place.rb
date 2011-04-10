edb385bc76734baec56614f7d6ca1cca1f2817c3
Alex <alex@alexgodin.com>
2011-04-10 08:02:26 -0400

    .

--- app/models/place.rb
+++ app/models/place.rb
@@ -2,11 +2,15 @@ class Place < ActiveRecord::Base
   has_many :messages
   def self.create_inc(place_id)
     create! do |user|
-      user.square_id = place_id
-      if Place.last != nil
-        user.room_id = Place.last.room_id+1
+      if self.find_by_square_id(place_id) = nil
+        user.square_id = place_id
+        if Place.last != nil
+          user.room_id = Place.last.room_id+1
+        else
+          user.room_id = 1111
+        end
       else
-        user.room_id = 1111
+        return self.find_by_square_id(place_id)
       end
     end
   end
